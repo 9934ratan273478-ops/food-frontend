4a65dab10b4e8722afd330b25b70a1425955a272
Raj Ratan <9934ratan273478@gmail.com>
2026-07-01 18:26:26 +0530

    hii

--- src/Components/OurMenu.jsx
+++ src/Components/OurMenu.jsx
@@ -1,6 +1,12 @@
 import "../Components/OurMenu.css"
+import { useNavigate } from "react-router-dom"
 import { GiCoffeeCup } from "react-icons/gi";
+import { BiBowlRice } from "react-icons/bi";
+import { FaGlassCheers } from "react-icons/fa";
+import { GiCupcake } from "react-icons/gi";
+
 function OurMenu() {
+  const navigate = useNavigate()
   return (
     <>
       <div className="Menu-div">
@@ -12,36 +18,35 @@ function OurMenu() {
             </div>
             <p className="head">Breakfast</p>
             <p className="para">In the new era of technology we look in the future with certainty and pride for our life.</p>
-            <button className="explore-btn">Explore Menu</button>
+            <button className="explore-btn" onClick={()=> navigate("/menuhero")}>Explore Menu</button>
           </div>
           <div className="card">
             <div className="icon-div">
-              <GiCoffeeCup size={30} />
+              <BiBowlRice  size={30} />
             </div>
-            <p className="head">Breakfast</p>
+            <p className="head">Main Dishes</p>
             <p className="para">In the new era of technology we look in the future with certainty and pride for our life.</p>
-            <button className="explore-btn">Explore Menu</button>
+            <button className="explore-btn"onClick={()=> navigate("/menuhero")} >Explore Menu</button>
           </div>
           <div className="card">
             <div className="icon-div">
-              <GiCoffeeCup size={30} />
+              <FaGlassCheers  size={30} />
             </div>
-            <p className="head">Breakfast</p>
+            <p className="head">Drinks</p>
             <p className="para">In the new era of technology we look in the future with certainty and pride for our life.</p>
-            <button className="explore-btn">Explore Menu</button>
+            <button className="explore-btn" onClick={()=> navigate("/menuhero")}>Explore Menu</button>
           </div>
           <div className="card">
             <div className="icon-div">
-              <GiCoffeeCup size={30} />
+              <GiCupcake size={30} />
             </div>
-            <p className="head">Breakfast</p>
+            <p className="head">Desserts</p>
             <p className="para">In the new era of technology we look in the future with certainty and pride for our life.</p>
-            <button className="explore-btn">Explore Menu</button>
+            <button className="explore-btn" onClick={()=> navigate("/menuhero")}>Explore Menu</button>
           </div>
         </div>
       </div>
     </>
   )
 }
-
 export default OurMenu
--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,15 +1,56 @@
 import "./Video.css"
-import video from "../assets/bg-video.mp4"
-function Video() {
-  return (
-    <>
-      <div className="Video-main">
-        <div className="video-div">
-          <video className="video" controls autoPlay loop src={video}></video>
-        </div>
-      </div>
-    </>
-  )
-}
+import { BiSolidFoodMenu } from "react-icons/bi";
+import { PiOvenDuotone } from "react-icons/pi";
+import { GiAlarmClock } from "react-icons/gi";
+import img from "../assets/playbutton.png"
+
+
+function Video(){
+
+    return(
+        <>
+   <div className="videoimg">
+    <img className="video-icon" src={img} alt="" />
+    <p className="video-para">Feel the authentic & <br /> original taste from us</p>
+    
+   </div>
+   <div className="video-row">
+   <div className="video-opt">
+    <div >
+        <BiSolidFoodMenu size={50} />
+       </div>
+    <div className="">
+        <h4 className="video-head">Multi Cuisine</h4>
+        <p>In the new era of technology we look in the future with certainty life.</p>
+    </div>
+    
+    
+   </div>
+   <div className="video-opt">
+    <div >
+        <PiOvenDuotone  size={50} />
+       </div>
+    <div className="">
+        <h4 className="video-head">Easy To Order</h4>
+        <p>In the new era of technology we look in the future with certainty life.</p>
+    </div>
+    
+    
+   </div>
+   <div className="video-opt">
+    <div >
+        <GiAlarmClock  size={50} />
+       </div>
+    <div className="">
+        <h4 className="video-head">Fast Delivery</h4>
+        <p>In the new era of technology we look in the future with certainty life.</p>
+    </div>
+    
+    
+   </div>
+   </div>
+   </>
+    )
+  }
 
 export default Video
